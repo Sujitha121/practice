4c9cccdb85a62c4e91eaf605cc42487db7ba376d
Sujitha R <sujitha.r@s.amity.edu>
2025-07-21 10:38:03 +0530

    C5

--- test.c
+++ test.c
@@ -8,8 +8,13 @@ int main() {
     cin >> grade;
     cin >> grade;
 
-    switch (grade) {
+    switch (graed) {
         case 'A':
         case 'a':
-            cout << "Excellent!" << endl;
+            cout << "Excelelnt!" << endl;
             break;
+	    case 'B':
+        case 'b':
+            cout << "Very Good!" << endl;
+            break;
+
